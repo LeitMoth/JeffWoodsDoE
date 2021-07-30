2824e6a62063898546ae982acfe697f30c23b1b3
Tizza123 <jp.staton1234@gmail.com>
2021-07-29 21:34:10 -0500

    Credit Changes

--- src/main/java/jeffgame/states/StateCredits.java
+++ src/main/java/jeffgame/states/StateCredits.java
@@ -26,7 +26,7 @@ public class StateCredits implements IGameState {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        cam = new Camera();
+            cam = new Camera();
 
         backTex = ResourceStore.getTexture("/texture/credit1.png");
 
